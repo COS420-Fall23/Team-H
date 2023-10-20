4af1386819bc853d476b7240406ac14ee0839660
Matthew Patterson <matthew.patterson@maine.edu>
2023-10-20 14:41:24 -0400

    Progress on Tutorial Scene
    Started the UI and Behavior Script

---
+++ Infinity Center/Assets/_Scripts/TutorialUIGuideBehavior.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TutorialUIGuideBehavior : MonoBehaviour
+{
+    // Canvas Objects
+    [SerializeField] private GameObject _welcomeScreen;
+    [SerializeField] private TMP_Text _topicTitle;
+    [SerializeField] private TMP_Text _bodyText;
+    
+    
+}
